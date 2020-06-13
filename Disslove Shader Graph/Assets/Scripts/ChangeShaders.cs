b88848d65724abbb52d464dada71b1d14237be6c
chocolade1972 <30846123+chocolade1972@users.noreply.github.com>
2020-06-14 00:48:32 +0300

    Add empty project

---
+++ Disslove Shader Graph/Assets/Scripts/ChangeShaders.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShaders : MonoBehaviour
+{
+    public Shader _shader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeShader();
+    }
+
+    private void ChangeShader()
+    {     
+        gameObject.GetComponent<Renderer>().material.shader = _shader;
+    }
+}
